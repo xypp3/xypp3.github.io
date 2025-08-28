491388f8367c1beb32695c1bae7da01c733f8527
xypp3 <petrkmilev@gmail.com>
2025-08-28 14:31:20 +0800

    THEMEING
    themeing wip
    
    more styling
    
    more blog theming
    
    more themeing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://xypp3.github.io',
-	base: 'xypp3.github.io',
-});
+// export default defineConfig({
+// 	site: 'https://xypp3.github.io',
+// 	base: 'xypp3.github.io',
+// });
--- src/content.config.ts
+++ src/content.config.ts
@@ -7,13 +7,13 @@ const blog = defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
     schema: z.object({
         title: z.string(),
-        pubDate: z.date(),
+        pubDate: z.coerce.date(),
         description: z.string(),
         author: z.string(),
         image: z.object({
             url: z.string(),
             alt: z.string()
-        }),
+        }).optional(),
         tags: z.array(z.string())
     })
 });
